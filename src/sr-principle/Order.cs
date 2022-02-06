89d7b942b48174067dfde93906fca82de657fb04
Begli NAZAROV <bndevops@hotmail.com>
2022-02-06 23:06:34 +0300

    problem defination

--- src/sr-principle/Order.cs
+++ src/sr-principle/Order.cs
@@ -2,15 +2,26 @@ namespace SingleResponsibilityPrinciple
 {
     public class Order
     {
+        /*
+            1-
+        */
         public int OrderId { get; set; }
 
         public string? ProductName { get; set; }
+
+        public string? ProductDesc { get; set; }
+
+        public decimal Price { get; set; }
 
         public string? CustomerName { get; set; }
 
         public string? CustomerAdress { get; set; }
 
         public string? CustomerAdressOfWork { get; set; }
+
+        public decimal? WalletBalance { get; set; }
+
+        public string? CouponCode { get; set; }
 
         public DateTime OrderDateTime { get; set; }
 
@@ -19,8 +30,16 @@ namespace SingleResponsibilityPrinciple
             /* Scenario - Base Cases
             -> Product Details name - desc - price etc.
             -> Customer Details fname - lname etc.
-            -> Payment Details Cache - Credit Card - Wallet 
+            -> Payment Details Cash - Credit Card - Wallet 
             -> Shipping Details Fast Ship - Regular Ship - Free Ship 
+            */
+        }
+
+        public void OrderValidation(Order order)
+        {
+            /* Scenario - Base Cases
+            -> Product is still available in stock ?
+            -> Customer has an validated address.
             */
         }
     }
